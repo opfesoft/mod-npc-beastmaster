aabefaf6b001268c92407c1251038a6974fd993d
Stoabrogga <5532995-Stoabrogga@users.noreply.gitlab.com>
2021-06-14 22:53:21 +0200

    Remove AIName

--- data/sql/db-world/npc_beastmaster.sql
+++ data/sql/db-world/npc_beastmaster.sql
@@ -4,9 +4,9 @@
 SET
 @Entry      := 601026,
 @Model      := 26314, -- Northrend Worgen White
-@Name       := "White Fang",
-@Title      := "Beast Master",
-@Icon       := "Speak",
+@Name       := 'White Fang',
+@Title      := 'Beast Master',
+@Icon       := 'Speak',
 @GossipMenu := 0,
 @MinLevel   := 80,
 @MaxLevel   := 80,
@@ -17,13 +17,12 @@ SET
 @Type       := 7,
 @TypeFlags  := 0,
 @FlagsExtra := 2,
-@AIName     := "SmartAI",
-@Script     := "BeastMaster";
+@Script     := 'BeastMaster';
 
 -- NPC
 DELETE FROM creature_template WHERE entry = @Entry;
-INSERT INTO creature_template (entry, modelid1, name, subname, IconName, gossip_menu_id, minlevel, maxlevel, faction, npcflag, speed_walk, speed_run, scale, rank, unit_class, unit_flags, type, type_flags, InhabitType, RegenHealth, flags_extra, AiName, ScriptName) VALUES
-(@Entry, @Model, @Name, @Title, @Icon, @GossipMenu, @MinLevel, @MaxLevel, @Faction, @NPCFlag, 1, 1.14286, @Scale, @Rank, 1, 2, @Type, @TypeFlags, 3, 1, @FlagsExtra, @AIName, @Script);
+INSERT INTO creature_template (entry, modelid1, name, subname, IconName, gossip_menu_id, minlevel, maxlevel, faction, npcflag, speed_walk, speed_run, scale, rank, unit_class, unit_flags, type, type_flags, InhabitType, RegenHealth, flags_extra, ScriptName) VALUES
+(@Entry, @Model, @Name, @Title, @Icon, @GossipMenu, @MinLevel, @MaxLevel, @Faction, @NPCFlag, 1, 1.14286, @Scale, @Rank, 1, 2, @Type, @TypeFlags, 3, 1, @FlagsExtra, @Script);
 
 -- NPC EQUIPPED
 DELETE FROM `creature_equip_template` WHERE `CreatureID`=@Entry AND `ID`=1;
